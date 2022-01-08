a6ac79a0ebaad6f2961c8d46eb1dc9b0370b82a3
student_49384 <kamechans.2223@gmail.com>
2022-01-08 22:19:56 +0900

    logged_inのCSS変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,23 +2,23 @@
 
 @section('content')
     <h1>ログイン</h1>
-    
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
         <div>
-            <label>
-                ユーザー名：
-                <input type="text" name="name">
-            </label>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div>
+                    <label>
+                        ユーザー名：
+                        <input type="text" name="name">
+                    </label>
+                </div>
+                
+                <div>
+                    <label>
+                        パスワード：
+                        <input type="password" name="password">
+                    </label>
+                </div>
+                <input type="submit" value="ログイン">
+            </form>
         </div>
-        
-        <div>
-            <label>
-                パスワード：
-                <input type="password" name="password">
-            </label>
-        </div>
-        
-        <input type="submit" value="ログイン">
-    </form>
 @endsection
--- resources/views/layouts/logged_in.blade.php
+++ resources/views/layouts/logged_in.blade.php
@@ -2,16 +2,18 @@
 
 @section('header')
 <header>
-    <ul class="header_nav">
-        <li>マイクロブログ</li>
-        <li><a href="{{ route('posts.index') }}">投稿一覧</a></li>
-        <li class="header_left"><a href="{{ route('posts.create') }}">新規投稿</a></li>
-        <li class="header_right">
-            <form action="{{ route('logout') }}" method="POST">
-                @csrf
-                <input type="submit" value="ログアウト">
-            </form>
-        </li>
-    </ul>
+    <div class="content_center">
+        <ul class="header_nav">
+            <li>マイクロブログ</li>
+            <li><a href="{{ route('posts.index') }}">投稿一覧</a></li>
+            <li class="header_left"><a href="{{ route('posts.create') }}">新規投稿</a></li>
+            <li class="header_right">
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <input type="submit" value="ログアウト">
+                </form>
+            </li>
+        </ul>
+    </div>  
 </header>
 @endsection

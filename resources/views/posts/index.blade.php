3c79d1f8827772b0816ca1942f2e4835bbf08dfd
student_49384 <kamechans.2223@gmail.com>
2022-01-17 08:10:09 +0900

    タグの変更

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -59,29 +59,32 @@
             </ul>  
         </article>
     </main>
-        <article> class="content_center">
-            <section class="content_center">
-                <h2>オススメのユーザー</h2>
+        <article class="content_center">
+            <h2>オススメのユーザー</h2>
+                <section>        
                     <ul class="Index">
                         @forelse($recommended_users as $recommended_user)
-                    <li class="followed_width posts_border">
-                        <a href="{{ route('users.show', $recommended_user) }}">{{ $recommended_user->name }}さん</a>
-                        <form method="post" action="{{route('follows.store')}}" class="follow">
-                            @csrf
-                            <input type="hidden" name="follow_id" value="{{ $recommended_user->id }}">
-                            <input type="submit" class="button follow_button" value="フォローする">
-                        </form>
-                    </li>
-                @empty
-                    <li class="no_posts">他のユーザーが存在しません</li>
-                @endforelse
-            </ul>
-                <div class="Index followed">
-                    <p><a href="{{ route('follows.index') }}">フォローしているユーザー</a></p>
-                    <p class="space_p">／</p>
-                    <p><a href="{{ route('follower.index') }}">フォローされているユーザー</a></p>
-                    <p class="space_p">／</p>
-                    <p><a href="{{ route('followeach.index') }}">相互フォローしているユーザー</a></p>
-                </div>
-    </div>
+                            <li class="followed_width posts_border">
+                                <a href="{{ route('users.show', $recommended_user) }}">{{ $recommended_user->name }}さん</a>
+                                    <form method="post" action="{{route('follows.store')}}" class="follow">
+                                        @csrf
+                                        <input type="hidden" name="follow_id" value="{{ $recommended_user->id }}">
+                                        <input type="submit" class="button follow_button" value="フォローする">
+                                    </form>
+                            </li>
+                        @empty
+                            <li class="no_posts">他のユーザーが存在しません</li>
+                        @endforelse
+                    </ul>
+                </section>
+                <section>
+                    <div class="Index followed">
+                        <p><a href="{{ route('follows.index') }}">フォローしているユーザー</a></p>
+                        <p class="space_p">／</p>
+                        <p><a href="{{ route('follower.index') }}">フォローされているユーザー</a></p>
+                        <p class="space_p">／</p>
+                        <p><a href="{{ route('followeach.index') }}">相互フォローしているユーザー</a></p>
+                    </div>
+                </section>
+        </article>
 @endsection
